ceb8203f31e96a6462613a81367e0640b495547b
ErodeesFleurs <862959461@qq.com>
2024-12-06 16:11:34 +0800

    修改zed配置文件

--- home/zed/zed.nix
+++ home/zed/zed.nix
@@ -3,5 +3,17 @@
 {
   programs.zed-editor = {
     enable = true;
+    extensions = [
+      "tokyo-night"
+    ];
+    userSettings = {
+      buffer_font_family = "CaskaydiaCove Nerd Font Mono";
+      ui_font_family = "CaskaydiaCove Nerd Font Propo";
+      theme = {
+        mode = "system";
+        dark = "Tokyo Night";
+        light = "Tokyo Night Storm";
+      };
+    };
   };
 }
