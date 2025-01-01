80463e4f6fc36f1b22d9e5b49ad7b0aef56ecc6e
ErodeesFleurs <862959461@qq.com>
2025-01-01 16:26:37 +0800

    修改使用fhs

--- home/openstarbound/default.nix
+++ home/openstarbound/default.nix
@@ -8,10 +8,31 @@
 
 let
   osb = pkgs.callPackage ./osb { };
-
+  osb-fhs = (
+    let
+      base = pkgs.appimageTools.defaultFhsEnvArgs;
+    in
+    pkgs.buildFHSUserEnv (
+      base
+      // {
+        name = "osb-fhs";
+        targetPkgs =
+          pkgs:
+          (base.targetPkgs pkgs)
+          ++ (with pkgs; [
+            pkg-config
+            ncurses
+          ]);
+        profile = "export FHS=1";
+        runScript = "fish";
+        extraOutputsToInstall = [ "dev" ];
+      }
+    )
+  );
 in
 {
   home.packages = with pkgs; [
-    osb
+    # osb
+    osb-fhs
   ];
 }
