23a7b01242c41f9e5d5b160b25521a0cbcf927d7
ErodeesFleurs <862959461@qq.com>
2025-01-04 13:59:59 +0800

    修改helix配置

--- home/helix/helix.nix
+++ home/helix/helix.nix
@@ -1,11 +1,48 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   # git 相关配置
   programs.helix = {
     enable = true;
     settings = {
-      theme = "tokyonight";
+    theme = "tokyonight";
+      editor = {
+        line-number = "relative";
+        mouse = true;
+        cursorline = true;
+        cursorcolumn = false;
+        bufferline = "multiple";
+        
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+
+        statusline = {
+          left = [
+            "mode"
+            "spinner"
+          ];
+          center = [
+            "file-name"
+          ];
+          right = [
+            "diagnostics"
+            "selections"
+            "position"
+            "file-encoding"
+            "file-line-ending"
+            "file-type"
+          ];
+          separator = "|";
+          mode = {
+            normal = "NORMAL";
+            insert = "INSERT";
+            select = "SELECT";
+          };
+        };
+      };
     };
   };
 }
