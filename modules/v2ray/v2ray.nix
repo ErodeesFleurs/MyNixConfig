31328eaac6069c7d6769a87a26d5ed6937e9962d
ErodeesFleurs <862959461@qq.com>
2025-01-12 11:39:48 +0800

    更新

--- modules/v2ray/v2ray.nix
+++ modules/v2ray/v2ray.nix
@@ -4,6 +4,23 @@
   services = {
     v2ray = {
       enable = true;
+      config = {
+        inbounds = [
+          {
+            listen = "127.0.0.1";
+            port = 1080;
+            protocol = "socks";
+            settings = {
+              udp = true;
+            };
+          }
+        ];
+        outbounds = [
+          {
+            protocol = "freedom";
+          }
+        ];
+      };
     };
     v2raya = {
       enable = true;
