0f00b5269fc2678e1503dbf0efaaf417d2e50a45
ErodeesFleurs <862959461@qq.com>
2025-03-11 11:01:26 +0800

    添加vsfsd

--- modules/vmware/vmware.nix
+++ modules/vmware/vmware.nix
@@ -18,6 +18,10 @@
   virtualisation = {
     libvirtd = {
       enable = true;
+      qemu = { 
+        vhostUserPackages = with pkgs; 
+          [ virtiofsd ];
+      };
     };
     spiceUSBRedirection = {
       enable = true;
