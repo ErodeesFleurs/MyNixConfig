52b91e1fa66579cf9247a88553741e5441283f31
ErodeesFleurs <862959461@qq.com>
2025-03-28 15:06:24 +0800

    添加镜像

--- modules/vmware/docker.nix
+++ modules/vmware/docker.nix
@@ -10,6 +10,13 @@
             "8.8.8.8"
             "114.114.114.114"
           ];
+          registry-mirrors = [
+            "https://registry.docker-cn.com"
+            "https://docker.mirrors.ustc.edu.cn"
+            "https://hub-mirror.c.163.com"
+            "https://mirror.baidubce.com"
+            "https://ccr.ccs.tencentyun.com"
+          ];
         };
       };
     };
