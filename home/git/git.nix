3aee1f0abe74a1ea416dadd70338a0dc44cc4e71
ErodeesFleurs <862959461@qq.com>
2025-10-30 10:26:46 +0800

    oops

--- home/git/git.nix
+++ home/git/git.nix
@@ -1,7 +1,13 @@
 { ... }:
 
 {
-  programs.gitui = {
+  programs.git = {
     enable = true;
+    settings = {
+      user = {
+        email = "862959461@qq.com";
+        name = "ErodeesFleurs";
+      };
+    };
   };
 }
