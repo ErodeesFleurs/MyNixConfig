3d3eb988098f68e54b57a4dd588b673b81fe0c22
ErodeesFleurs <862959461@qq.com>
2025-11-24 09:16:15 +0800

    update for stylix

--- home/vicinae/vicinae.nix
+++ home/vicinae/vicinae.nix
@@ -1,15 +1,15 @@
-{ config, ... }:
+{ ... }:
 
-let
-  themeName = if config.stylix.polarity == "dark" then "gruvbox-dark" else "gruvbox-light";
-in
+# let
+# themeName = if config.stylix.polarity == "dark" then "gruvbox-dark" else "gruvbox-light";
+# in
 {
   services.vicinae = {
     enable = true;
     autoStart = true;
     useLayerShell = false;
     settings = {
-      theme.name = themeName;
+      # theme.name = themeName;
     };
   };
 }
